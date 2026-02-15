ef36d3eeaf45961e6ef323f0621fe859d0355a98
Ayush Thakur <thakurayush9550@gmail.com>
2026-02-15 20:19:10 +0530

    Update App.tsx
    Signed-off-by: Ayush Thakur <thakurayush9550@gmail.com>

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import { SafeAreaView, ScrollView } from 'react-native';
 import React from 'react';
 import FlatCard from './components/FlatCard';
 import ElevatedCard from './components/ElevatedCard';
+import FancyCard from './components/FancyCard';
+import ActionCard from './components/ActionCard';
+import ContactList from './components/ContactList';
 
 const App = () => {
   return (
@@ -9,7 +12,10 @@ const App = () => {
       <ScrollView>
         <FlatCard />
         <ElevatedCard />
+        <FancyCard />
+        <ActionCard />
       </ScrollView>
+      <ContactList />
     </SafeAreaView>
   );
 };
